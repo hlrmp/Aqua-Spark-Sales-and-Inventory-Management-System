903cdd01d0b3ecd8ed783318cc3c4e70d0930cc3
hlrmp <haysjhune@gmail.com>
2023-05-29 19:15:16 +0800

    2

--- Aqua Spark Sales and Inventory Management System/log in window.cs
+++ Aqua Spark Sales and Inventory Management System/log in window.cs
@@ -38,83 +38,90 @@ namespace Aqua_Spark_Sales_and_Inventory_Management_System
             }
             else
             {
-               
-
-                try
-                {
-
-                    string username, password;
-
-                    username = textBox1_username.Text;
-                    password = textBox2_password.Text;
-
-
-                    string quer = "SELECT * FROM log in WHERE user name = '" + textBox1_username.Text + "' AND password = '" + textBox2_password.Text + "'";
-                    //  SqlDataAdapter sd = new SqlDataAdapter(quer,conn);
-
-                    DataTable table = new DataTable(quer);
-                    //sd.Fill(table);
-
-                    //string querlognum = "SELECT * FROM log in WHERE lid IN('1')"; // user name and password 1 
-                    //string querlognum2 = "SELECT * FROM log in WHERE lid IN('1')"; // user name and password 2
-                    //   string queruser = " SELECT USER NAME FROM log in WHERE condition";
-
-                    int c = 1;
-                    int a = 1;
-
-                    if (table.Rows.Count >= 0 )
-                    {
-                        username = textBox1_username.Text;
-                        password = textBox2_password.Text;
-
-                        textBox1_username.Clear();
-                        textBox2_password.Clear();
-
-                        MessageBox.Show("log in succesfully", "thank you", MessageBoxButtons.OK);
-
-                        //  if (username == querlognum && password == querlognum)
-                        // { // if user name & pass == to cashier
-
-
-                        //string querlogcount = " INSERT INTO CASHIER ACC (LOG COUNT) VALUES(c);";
-                        //    }
-
-                        //  if (username == querlognum2 && password == querlognum2) // if user name & pass == to admin
-                        //  {
-
-                        //       string querlogcount2 = " INSERT INTO ADMIN ACC (LOG COUNT) VALUES(a);";
-                        //  }
-
-                        main_window fmain = new main_window();    // main window instance
-
-                        fmain.Show();   // show main form 
-                        this.Hide();   // hide this form 
-
-
-                    }
-                    else
-                    {
-                        MessageBox.Show("user name or password incorrect", "error", MessageBoxButtons.OK);
-                        textBox1_username.Clear();
-                        textBox2_password.Clear();
-
-                    }
-
-                }
-                catch (Exception )
-                {
-                    
-                 
-                    
-                        MessageBox.Show("error", "error", MessageBoxButtons.OK);
-                      
-              
-                }
-                //     finally
-                //    {
-                //         conn.Close();
-                //    }
+                main_window fmain = new main_window();    // main window instance
+                fmain.Show();   // show main form 
+                this.Hide();   // hide this form 
             }
+
+
+            /*     try
+                 {
+
+                     string username, password;
+
+                     username = textBox1_username.Text;
+                     password = textBox2_password.Text;
+
+
+                     string quer = "SELECT * FROM log in WHERE user name = '" + textBox1_username.Text + "' AND password = '" + textBox2_password.Text + "'";
+                     SqlDataAdapter sd = new SqlDataAdapter(quer,conn);
+
+                     DataTable table = new DataTable(quer);
+                     sd.Fill(table);
+
+                     //string querlognum = "SELECT * FROM log in WHERE lid IN('1')"; // user name and password 1 
+                     //string querlognum2 = "SELECT * FROM log in WHERE lid IN('1')"; // user name and password 2
+                     //   string queruser = " SELECT USER NAME FROM log in WHERE condition";
+
+                     int c = 1;
+                     int a = 1;
+
+
+                     if (table.Rows.Count > 0 )
+                     {
+                         username = textBox1_username.Text;
+                         password = textBox2_password.Text;
+
+                         textBox1_username.Clear();
+                         textBox2_password.Clear();
+
+                         MessageBox.Show("log in succesfully", "thank you", MessageBoxButtons.OK);
+
+                         //  if (username == querlognum && password == querlognum)
+                         // { // if user name & pass == to cashier
+
+
+                         //string querlogcount = " INSERT INTO CASHIER ACC (LOG COUNT) VALUES(c);";
+                         //    }
+
+                         //  if (username == querlognum2 && password == querlognum2) // if user name & pass == to admin
+                         //  {
+
+                         //       string querlogcount2 = " INSERT INTO ADMIN ACC (LOG COUNT) VALUES(a);";
+                         //  }
+
+                         main_window fmain = new main_window();    // main window instance
+
+                         fmain.Show();   // show main form 
+                         this.Hide();   // hide this form 
+
+
+                     }
+                     else
+                     {
+                         MessageBox.Show("user name or password incorrect", "error", MessageBoxButtons.OK);
+                         textBox1_username.Clear();
+                         textBox2_password.Clear();
+
+                     }
+
+                 }
+                 catch (Exception )
+                 {
+
+
+
+                         MessageBox.Show("error", "error", MessageBoxButtons.OK);
+
+
+                 }
+                 //     finally
+                 //    {
+                 //         conn.Close();
+                 //    }
+             }
+
+                  */
 
         }  // log in button 
 
--- Aqua Spark Sales and Inventory Management System/main window.Designer.cs
+++ Aqua Spark Sales and Inventory Management System/main window.Designer.cs
@@ -28,13 +28,108 @@
         /// </summary>
         private void InitializeComponent()
         {
+            button5 = new Button();
+            listBox1 = new ListBox();
+            button1 = new Button();
+            button4 = new Button();
+            button2 = new Button();
+            button8 = new Button();
+            button3 = new Button();
             SuspendLayout();
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.FromArgb(128, 128, 255);
+            button5.Location = new Point(59, 95);
+            button5.Name = "button5";
+            button5.Size = new Size(89, 56);
+            button5.TabIndex = 18;
+            button5.Text = "items";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // listBox1
+            // 
+            listBox1.Anchor = AnchorStyles.Top;
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 15;
+            listBox1.Location = new Point(213, 67);
+            listBox1.MaximumSize = new Size(10000, 10000);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(381, 334);
+            listBox1.TabIndex = 21;
+            // 
+            // button1
+            // 
+            button1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button1.BackColor = Color.FromArgb(255, 192, 128);
+            button1.Location = new Point(661, 66);
+            button1.Name = "button1";
+            button1.Size = new Size(127, 34);
+            button1.TabIndex = 22;
+            button1.Text = "orders";
+            button1.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button4.BackColor = Color.FromArgb(255, 192, 128);
+            button4.Location = new Point(661, 141);
+            button4.Name = "button4";
+            button4.Size = new Size(127, 34);
+            button4.TabIndex = 23;
+            button4.Text = "inventory";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button2.BackColor = Color.FromArgb(255, 192, 128);
+            button2.Location = new Point(661, 221);
+            button2.Name = "button2";
+            button2.Size = new Size(127, 34);
+            button2.TabIndex = 24;
+            button2.Text = "sales info";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            button8.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button8.BackColor = Color.Red;
+            button8.Font = new Font("Stencil", 9F, FontStyle.Regular, GraphicsUnit.Point);
+            button8.Location = new Point(661, 367);
+            button8.Name = "button8";
+            button8.Size = new Size(127, 34);
+            button8.TabIndex = 26;
+            button8.Text = "log out";
+            button8.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.Anchor = AnchorStyles.Top | AnchorStyles.Right;
+            button3.BackColor = Color.FromArgb(255, 192, 128);
+            button3.Location = new Point(661, 298);
+            button3.Name = "button3";
+            button3.Size = new Size(127, 34);
+            button3.TabIndex = 25;
+            button3.Text = "Customers";
+            button3.UseVisualStyleBackColor = false;
             // 
             // main_window
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = Properties.Resources._int;
+            BackgroundImageLayout = ImageLayout.Stretch;
             ClientSize = new Size(800, 450);
+            Controls.Add(button8);
+            Controls.Add(button3);
+            Controls.Add(button2);
+            Controls.Add(button4);
+            Controls.Add(button1);
+            Controls.Add(listBox1);
+            Controls.Add(button5);
+            DoubleBuffered = true;
+            FormBorderStyle = FormBorderStyle.Fixed3D;
             Name = "main_window";
             Text = "main_window";
             WindowState = FormWindowState.Maximized;
@@ -42,5 +137,13 @@
         }
 
         #endregion
+
+        private Button button5;
+        private ListBox listBox1;
+        private Button button1;
+        private Button button4;
+        private Button button2;
+        private Button button8;
+        private Button button3;
     }
 }
