3caeeaba69e76916eae6030f91d2a2d98a27bc25
hlrmp <haysjhune@gmail.com>
2023-06-06 02:01:13 +0800

    3.0.6.2

--- Aqua Spark Sales and Inventory Management System/Orders Window.Designer.cs
+++ Aqua Spark Sales and Inventory Management System/Orders Window.Designer.cs
@@ -35,18 +35,22 @@
             pictureBox1 = new PictureBox();
             button2 = new Button();
             button3 = new Button();
+            pictureBox2 = new PictureBox();
             ((System.ComponentModel.ISupportInitialize)dataGridView_order).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
             SuspendLayout();
             // 
             // button1
             // 
             button1.Anchor = AnchorStyles.Left;
-            button1.BackColor = Color.FromArgb(0, 192, 192);
-            button1.Font = new Font("Stencil", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            button1.Location = new Point(46, 74);
+            button1.BackColor = Color.Transparent;
+            button1.FlatStyle = FlatStyle.Popup;
+            button1.Font = new Font("Stencil", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button1.ForeColor = SystemColors.ButtonFace;
+            button1.Location = new Point(46, 66);
             button1.Name = "button1";
-            button1.Size = new Size(127, 60);
+            button1.Size = new Size(184, 65);
             button1.TabIndex = 24;
             button1.Text = "HOME";
             button1.UseVisualStyleBackColor = false;
@@ -57,7 +61,7 @@
             dataGridView_order.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
             dataGridView_order.BackgroundColor = SystemColors.ControlLight;
             dataGridView_order.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dataGridView_order.Location = new Point(244, 144);
+            dataGridView_order.Location = new Point(296, 134);
             dataGridView_order.Name = "dataGridView_order";
             dataGridView_order.RowTemplate.Height = 25;
             dataGridView_order.Size = new Size(409, 232);
@@ -68,12 +72,12 @@
             label1.Anchor = AnchorStyles.Top;
             label1.AutoSize = true;
             label1.BackColor = Color.Transparent;
-            label1.Font = new Font("Stencil", 26.25F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.ForeColor = Color.White;
-            label1.Location = new Point(237, 0);
+            label1.Font = new Font("Stencil", 36F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.Cyan;
+            label1.Location = new Point(276, 9);
             label1.Margin = new Padding(4, 0, 4, 0);
             label1.Name = "label1";
-            label1.Size = new Size(374, 42);
+            label1.Size = new Size(513, 57);
             label1.TabIndex = 30;
             label1.Text = "aqua spark orders";
             label1.TextAlign = ContentAlignment.TopCenter;
@@ -82,7 +86,7 @@
             // 
             textBox1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             textBox1.BorderStyle = BorderStyle.None;
-            textBox1.Location = new Point(490, 71);
+            textBox1.Location = new Point(490, 80);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
             textBox1.Size = new Size(237, 23);
@@ -92,7 +96,7 @@
             // 
             pictureBox1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             pictureBox1.Image = Properties.Resources.search;
-            pictureBox1.Location = new Point(733, 71);
+            pictureBox1.Location = new Point(733, 80);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(29, 23);
             pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
@@ -103,11 +107,13 @@
             // button2
             // 
             button2.Anchor = AnchorStyles.Left;
-            button2.BackColor = Color.FromArgb(255, 192, 128);
-            button2.Font = new Font("Stencil", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            button2.Location = new Point(46, 193);
+            button2.BackColor = Color.Transparent;
+            button2.FlatStyle = FlatStyle.Popup;
+            button2.Font = new Font("Stencil", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button2.ForeColor = SystemColors.ButtonFace;
+            button2.Location = new Point(46, 187);
             button2.Name = "button2";
-            button2.Size = new Size(127, 60);
+            button2.Size = new Size(184, 65);
             button2.TabIndex = 33;
             button2.Text = "Add orders";
             button2.UseVisualStyleBackColor = false;
@@ -116,14 +122,26 @@
             // button3
             // 
             button3.Anchor = AnchorStyles.Left;
-            button3.BackColor = Color.FromArgb(255, 192, 128);
-            button3.Font = new Font("Stencil", 14.25F, FontStyle.Regular, GraphicsUnit.Point);
-            button3.Location = new Point(46, 316);
+            button3.BackColor = Color.Transparent;
+            button3.FlatStyle = FlatStyle.Popup;
+            button3.Font = new Font("Stencil", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            button3.ForeColor = SystemColors.ButtonFace;
+            button3.Location = new Point(46, 306);
             button3.Name = "button3";
-            button3.Size = new Size(127, 60);
+            button3.Size = new Size(184, 60);
             button3.TabIndex = 34;
             button3.Text = "edit";
             button3.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left;
+            pictureBox2.BackColor = Color.CadetBlue;
+            pictureBox2.Location = new Point(-2, 0);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(271, 452);
+            pictureBox2.TabIndex = 42;
+            pictureBox2.TabStop = false;
             // 
             // Orders_Window
             // 
@@ -139,6 +157,7 @@
             Controls.Add(label1);
             Controls.Add(dataGridView_order);
             Controls.Add(button1);
+            Controls.Add(pictureBox2);
             DoubleBuffered = true;
             FormBorderStyle = FormBorderStyle.None;
             Name = "Orders_Window";
@@ -146,6 +165,7 @@
             WindowState = FormWindowState.Maximized;
             ((System.ComponentModel.ISupportInitialize)dataGridView_order).EndInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -159,5 +179,6 @@
         private PictureBox pictureBox1;
         private Button button2;
         private Button button3;
+        private PictureBox pictureBox2;
     }
 }
