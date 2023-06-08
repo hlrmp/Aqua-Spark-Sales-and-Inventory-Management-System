06c4a7452021cea5f5758e423d2476c836bbd3a6
hlrmp <134066063+hlrmp@users.noreply.github.com>
2023-06-09 04:53:25 +0800

    Add files via upload

--- Aqua Spark Sales and Inventory Management System/connection class.cs
+++ Aqua Spark Sales and Inventory Management System/connection class.cs
@@ -1,23 +1,28 @@
-﻿using System;
+﻿using Microsoft.Data.SqlClient;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 using System.Text;
 using System.Threading.Tasks;
 using System.Web;
+using System.Data.SqlClient;
 
-namespace Aqua_Spark_Sales_and_Inventory_Management_System
+namespace Aqua_Spark
 {
     internal class connection_class
     {
 
-        public string cn(string connection)
+        public string Connection()
         {
-             connection = "Data Source=LAPTOP-3JRG23SL;Initial Catalog=aquasparkdata;Integrated Security=True";
-
-            return connection;
-
-        }
+        public string conn = "@Data Source=LAPTOP-3JRG23SL;Initial Catalog=aquasparkdata;Integrated Security=True";
+        return conn;
+        
     }
+      
+    }
+}  
 
+  
 
-}
