36c027b21d6447e7f8d45957f9d6b252f722c36c
hlrmp <haysjhune@gmail.com>
2023-06-22 12:23:31 +0800

    sd

--- Aqua Spark Sales and Inventory Management System/supplier.cs
+++ Aqua Spark Sales and Inventory Management System/supplier.cs
@@ -17,40 +17,43 @@ namespace Aqua_Spark_Sales_and_Inventory_Management_System
         {
             InitializeComponent();
         }
+
+        public string fn = textBoxfname.Text;
+        public string ln = textBoxlname.Text;
+
         connection_class cn = new connection_class();
         private void button1_Click(object sender, EventArgs e)
         {
-
-
+           
 
             try
             {
 
-               
+
 
                 using (SqlConnection cnn = new SqlConnection(cn.conn))
                 {
                     cnn.Open();
-                      string quer = " INSERT INTO staffs (first_name ,last_name,email,contact_number ) VALUES (@first_name ,@last_name,@email,@contact_number)";
-                   
+                    string quer = " INSERT INTO staffs (first_name ,last_name,email,contact_number ) VALUES (@first_name ,@last_name,@email,@contact_number)";
 
-                  //  string quer1 = " INSERT INTO staffs (first_name ,last_name,email,contact_number ) VALUES ('" + textBoxfname + "' ,'" + textBoxlname + "','" + textBoxcontactno + "','" + textBoxemail + "')";
-                  //  SqlCommand cmm = new SqlCommand(quer1, cnn);
-                      SqlCommand command = new SqlCommand(quer, cnn);
 
-                    command.Parameters.AddWithValue("@first_name",textBoxfname.Text);
-                     command.Parameters.AddWithValue("@last_name",textBoxlname.Text);
-                     command.Parameters.AddWithValue("@email",textBoxemail.Text);
-                     command.Parameters.AddWithValue("@contact_number",textBoxcontactno.Text);
-                   
-                     command.ExecuteNonQuery();
+                    //  string quer1 = " INSERT INTO staffs (first_name ,last_name,email,contact_number ) VALUES ('" + textBoxfname + "' ,'" + textBoxlname + "','" + textBoxcontactno + "','" + textBoxemail + "')";
+                    //  SqlCommand cmm = new SqlCommand(quer1, cnn);
+                    SqlCommand command = new SqlCommand(quer, cnn);
+
+                    command.Parameters.AddWithValue("@first_name", textBoxfname.Text);
+                    command.Parameters.AddWithValue("@last_name", textBoxlname.Text);
+                    command.Parameters.AddWithValue("@email", textBoxemail.Text);
+                    command.Parameters.AddWithValue("@contact_number", textBoxcontactno.Text);
+
+                    command.ExecuteNonQuery();
 
                     cnn.Close();
 
 
                 }
 
-              
+
 
 
             }
@@ -61,14 +64,14 @@ namespace Aqua_Spark_Sales_and_Inventory_Management_System
 
 
 
-          
+
         }
 
         private void button2_Click(object sender, EventArgs e) // cancell button
         {
             textBoxlname.Clear();
             textBoxfname.Clear();
-         
+
             textBoxcontactno.Clear();
             textBoxemail.Clear();
 
